8ca989f0ba071be77267236a48a07e08638c978e
Jett Thompson <jett.thompson.59@gmail.com>
2022-03-19 18:17:52 +1100

    made it find prime numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::fs;
+use std::time::Instant;
+use std::vec;
+
 fn main() {
-    println!("Hello, world!");
+    let mut primes: Vec<u64> = vec![2u64];
+    let start = Instant::now();
+    find_primes(&mut primes, 200000000);
+    let time_taken = start.elapsed();
+    println!("It took {time_taken:.3?}");
+    let mut prime_txt = String::new();
+    for prime in primes {
+        prime_txt.push_str(&prime.to_string());
+        prime_txt.push('\n');
+    }
+    fs::write("primes.txt", prime_txt).expect("Something went wrong!");
+}
+
+fn find_primes(primes: &mut Vec<u64>, num: u64) {
+    for n in 3u64..num {
+        if is_prime(n, primes) {
+            primes.push(n);
+        }
+    }
+}
+
+fn is_prime(x: u64, primes: &Vec<u64>) -> bool {
+    let top = f64::sqrt(x as f64).floor() as u64;
+    for n in primes {
+        if (x % n) == 0 {
+            return false;
+        }
+        if n > &top {
+            return true;
+        }
+    }
+    return true;
 }
